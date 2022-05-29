accfd8cc930014c53b0aebd34677223cb4bc8cec
Wenming Pan <pwm@google.com>
2022-05-29 22:32:38 +0800

    Sun May 29 22:32:38 CST 2022

--- pubsub/myes.py
+++ pubsub/myes.py
@@ -1,6 +1,6 @@
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch("https://instance6.joey618.top:9200", basic_auth=('xxxx','xxxx'), verify_certs=False)
+es = Elasticsearch("https://instance6.joey618.top:9200", basic_auth=('xxxxx','xxxxx'), verify_certs=False)
 
 def insert(doc):
     result = es.index(index="glo-log-index", document=doc) 
